0e1a7f786aac56b2fc8ac6e47a9aecf136ed29b0
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-01-24 13:34:59 -0500

    MVP

--- Answers.md
+++ Answers.md
@@ -1,5 +1,22 @@
 1. What problem does the context API help solve?
+
+It helps with passing global variables around. No more prop drilling.
+
 1. In your own words, describe `actions`, `reducers` and the `store` and their role in Redux. What does each piece do? Why is the store known as a 'single source of truth' in a redux application?
+
+Action: It performs an action like deleting removing or toggling.
+Reducer: Takes an action and state and gives you an updated state.
+Store: Contains the state for an entire application.
+
 1. What is the difference between Application state and Component state? When would be a good time to use one over the other?
+
+App state: Encompasses the state of the whole application.
+Component state: Encompasses the state of a single component.
+
 1. Describe `redux-thunk`, what does it allow us to do? How does it change our `action-creators`?
+
+It makes things asynchronous. It allows us to make api calls from action creators.
+
 1. What is your favorite state management system you've learned and this sprint? Please explain why!
+
+I think redux because of centralized state even though I was too nervous to use it in todays project.
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,58 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SmurfsForm from "./SmurfsForm";
+import SmurfsList from "./SmurfsList";
+import NavBar from "./NavBar";
+
+import { SmurfContext } from "../contexts/SmurfContext";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(res => {
+        setSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const addSmurfs = smurf => {
+    axios
+      .post("http://localhost:3333/smurfs", smurf)
+      .then(res => {})
+      .catch(err => console.log(err));
+  };
+
+  const deleteSmurf = smurfId => {
+    axios
+      .delete(`http://localhost:3333/smurfs/${smurfId}`)
+      .then(res => {
+        setSmurfs(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <SmurfContext.Provider
+      value={{ smurfs, setSmurfs, addSmurfs, deleteSmurf }}
+    >
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <NavBar />
+        <section>
+          <div>
+            <SmurfsForm />
+            <div>
+              <h1 className="title">Create Your Smurf Village</h1>
+              <SmurfsList />
+            </div>
+          </div>
+        </section>
       </div>
-    );
-  }
-}
+    </SmurfContext.Provider>
+  );
+};
 
 export default App;
